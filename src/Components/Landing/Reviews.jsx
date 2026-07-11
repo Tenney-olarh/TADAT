e186cc65bde5c2948cb55ca1a5fddc8d29996034
tenney_olarh <alameenadetona04@gmail.com>
2026-07-11 11:20:36 +0100

    edited the reviews

--- src/Components/Landing/Reviews.jsx
+++ src/Components/Landing/Reviews.jsx
@@ -1,5 +1,7 @@
 import {Link} from "react-router-dom";
 import Shina from "../../assets/shina.png";
+import Okolie from "../../assets/okolie.png";
+import Chidi from "../../assets/chidi.png";
 const Reviews = () => {
   return (
     <div className="h-[350px] bg-[#F5F8FF] py-10 px-10">
@@ -7,10 +9,10 @@ const Reviews = () => {
         Backed by a growing community of 1,000+ Users
       </h1>
       <section className="flex flex-row mt-10 justify-around">
-        <div className="w-[382px] h-[186px] rounded-[8px] bg-[#FFFFFF] flex flex-col px-3 py-3 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
+        <div className="w-[382px] h-[186px] rounded-[8px] bg-[#FFFFFF] flex flex-col px-5 py-5 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
           <section className="flex flex-row gap-2">
             <Link>
-              <img src={Shina} alt="Shina" />
+              <img src={Shina} alt="Shina" className="object-cover" />
             </Link>
             <div>
               <h1 className="text-[16px] font-medium text-[#000000] leading-[100%] tracking-[0%] w-[171px] h-[19px]">
@@ -27,43 +29,43 @@ const Reviews = () => {
           </p>
         </div>
 
-        <div className="w-[382px] h-[186px ] rounded-[8px] bg-[#FFFFFF] flex flex-col px-3 py-3 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
+        <div className="w-[382px] h-[186px ] rounded-[8px] bg-[#FFFFFF] flex flex-col px-5 py-5 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
           <section className="flex flex-row gap-2">
             <Link>
-              <img src={Shina} alt="Shina" />
+              <img src={Okolie} alt="Okolie" className="object-cover" />
             </Link>
             <div>
               <h1 className="text-[16px] font-medium text-[#000000] leading-[100%] tracking-[0%] w-[171px] h-[19px]">
-                Olasunkanmi Ademola
+                Annette Okolie
               </h1>
               <p className="h-[20px] w-[138px] text-[14px] text-[#000000] leading-[100%] tracking-[0%]">
-                Freelancer
+                Graduate
               </p>
             </div>
           </section>
           <p className="w-[334px] h-[57px] italic text-[#000000] text-[16px] leading-[100%] tracking-[0%] mt-7">
-            "The transparency is what sold me. I know exactly how much each
-            action is worth. A truly modern diagnostic tool for my wallet."
+            "Best decision I've made for my digital life. No more expensive data
+            plans that I have to pay for out of pocket."
           </p>
         </div>
 
-        <div className="w-[382px] h-[186px ] rounded-[8px] bg-[#FFFFFF] flex flex-col px-3 py-3 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
+        <div className="w-[382px] h-[186px ] rounded-[8px] bg-[#FFFFFF] flex flex-col px-5 py-5 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
           <section className="flex flex-row gap-2">
             <Link>
-              <img src={Shina} alt="Shina" />
+              <img src={Chidi} alt="Chidi" className="object-cover" />
             </Link>
             <div>
               <h1 className="text-[16px] font-medium text-[#000000] leading-[100%] tracking-[0%] w-[171px] h-[19px]">
-                Olasunkanmi Ademola
+                Chidi Okechukwu
               </h1>
               <p className="h-[20px] w-[138px] text-[14px] text-[#000000] leading-[100%] tracking-[0%]">
-                Freelancer
+                Student
               </p>
             </div>
           </section>
           <p className="w-[334px] h-[57px] italic text-[#000000] text-[16px] leading-[100%] tracking-[0%] mt-7">
-            "The transparency is what sold me. I know exactly how much each
-            action is worth. A truly modern diagnostic tool for my wallet."
+            "TADAT has literally covered my monthly data costs just by engaging
+            in tasks I already do on a daily basis. It's effortless."
           </p>
         </div>
       </section>
