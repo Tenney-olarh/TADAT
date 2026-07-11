42aeb3e54e2a45462b66da3a95746d75dc491ce0
tenney_olarh <alameenadetona04@gmail.com>
2026-07-11 03:27:30 +0100

    imported assest needed for the reviews.jsx

--- src/Components/Landing/Home.jsx
+++ src/Components/Landing/Home.jsx
@@ -1,31 +1,31 @@
 import HomeImage from "../../assets/Rectangle 92.png";
-        const Home = () => (
-          <div className="flex flex-row justify-between bg-[#F5F8FF] px-10 py-10 mt-[80px]">
-            {/* LEFT HAND SIDE */}
-            <div className="flex flex-col max-w-[600px]">
-              <h1 className="font-normal text-[65px] tracking-[0%] text-[#000000] leading-[80px]">
-                Turn Your <br /> Engagement Into{" "}
-                <span className="text-[#1C7CDF]"> Mobile Data</span>
-              </h1>
-              <p className="w-[598px] h-[116px] font-extralight text-[24px] tracking-[0%] text-[#000000] leading-[100%] mt-[50px] mb-[50px]">
-                Turn your digital interactions into mobile data. <br />
-                Simply complete verified tasks on the{" "}
-                <span className="text-[#1C7CDF]">TADAT</span> to earn <br />{" "}
-                seamless data top-ups paid directly to your <br />
-                mobile carrier.
-              </p>
-              <button className="w-[130px] h-[38px] rounded-[8px] bg-[#1C7CDF] text-[#FFFFFF] font-medium cursor-pointer shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] hover:bg-[#1C7CDF]/80 pt-[0px]">
-                Get Started
-              </button>
-            </div>
-            {/* RIGHT HAND SIDE  */}
-            <div className="flex-shrink-0">
-              <img
-                src={HomeImage}
-                alt="Home"
-                className="w-[699px] h-[503px] object-cover"
-              />
-            </div>
-          </div>
-        );
-export default Home;  
+const Home = () => (
+  <div className="flex flex-row justify-between bg-[#F5F8FF] px-10 py-10 mt-[80px]">
+    {/* LEFT HAND SIDE */}
+    <div className="flex flex-col max-w-[600px]">
+      <h1 className="font-normal text-[65px] tracking-[0%] text-[#000000] leading-[80px]">
+        Turn Your <br /> Engagement Into{" "}
+        <span className="text-[#1C7CDF]"> Mobile Data</span>
+      </h1>
+      <p className="w-[598px] h-[116px] font-extralight text-[24px] tracking-[0%] text-[#000000] leading-[100%] mt-[50px] mb-[50px]">
+        Turn your digital interactions into mobile data. <br />
+        Simply complete verified tasks on the{" "}
+        <span className="text-[#1C7CDF]">TADAT</span> to earn <br /> seamless
+        data top-ups paid directly to your <br />
+        mobile carrier.
+      </p>
+      <button className="w-[130px] h-[38px] rounded-[8px] bg-[#1C7CDF] text-[#FFFFFF] font-medium cursor-pointer shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] hover:bg-[#1C7CDF]/80 pt-[0px]">
+        Get Started
+      </button>
+    </div>
+    {/* RIGHT HAND SIDE  */}
+    <div className="flex-shrink-0">
+      <img
+        src={HomeImage}
+        alt="Home"
+        className="w-[699px] h-[503px] object-cover"
+      />
+    </div>
+  </div>
+);
+export default Home;
--- src/Components/Landing/Reviews.jsx
+++ src/Components/Landing/Reviews.jsx
@@ -1,6 +1,13 @@
 const Reviews = () => {
     return (
-        <div></div>
-    )
+      <div className="h-[350px] bg-[#F5F8FF] py-10 px-10">
+        <h1 className="text-[36px] font-mediumleading text-[#000000]  h-[44px] leading-[100%] tracking-[0%] flex text-center justify-center">
+          Backed by a growing community of 1,000+ Users
+            </h1>
+            <section>
+                
+            </section>
+      </div>
+    );
 }
 export default Reviews;
