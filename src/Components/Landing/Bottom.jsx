afe8d7a68dcefc3a4480a6f2e91749222475e681
tenney_olarh <alameenadetona04@gmail.com>
2026-07-14 12:10:09 +0100

    corrected rearny,finished botom

--- src/Components/Landing/Bottom.jsx
+++ src/Components/Landing/Bottom.jsx
@@ -1,6 +1,52 @@
-const Bottom=() => {
-    return (
-          <div></div>
-    );
-}
+import { Link } from "react-router-dom";
+import Twitter from "../../assets/x_logo.svg.png";
+const Bottom = () => {
+  return (
+    <div className="w-full h-[286px] bg-[#F3F5FF] flex flex-row justify-around items-center p-10">
+      <section className="flex flex-col gap-5 w-[489px] h-[89px]">
+        <h1 className="text-[36px] leading-[100%] tracking-tight text-[#000000] font-medium">
+          TADAT
+        </h1>
+        <p className=" h-[19px] text-[16px] leading-[100%] tracking-[0%] text-[#0D1C2E] font-normal">
+          Empowering the digital world through value-based connectivity.
+        </p>
+      </section>
+
+      <div className="flex flex-col gap-5 w-[128px] h-[72px]">
+        <h1 className="w-[93px] h-[29px] text-[24px] leading-[100%] tracking-tight text-[#000000] font-medium">
+          Support
+        </h1>
+        <p className="w-[489px] h-[19px] text-[16px] leading-[100%] tracking-[0%] text-[#0D1C2E] font-normal">
+          Contact Support
+        </p>
+      </div>
+
+      <section className="flex flex-col gap-5 w-[130px] h-[75px]">
+        <h1 className="w-[63px] h-[29px] text-[24px] leading-[100%] tracking-tight text-[#000000] font-medium">
+          Legal
+        </h1>
+        <p className=" text-[16px] leading-[100%] tracking-[0%] text-[#0D1C2E] font-normal">
+          Privacy Policy
+        </p>
+        <p className=" text-[16px] leading-[100%] tracking-[0%] text-[#0D1C2E] font-normal">
+          Terms of Service
+        </p>
+      </section>
+
+      <div className="flex flex-col gap-5 w-[191px] h-[75px]">
+        <h1 className="w-[191px] h-[29px] text-[24px] leading-[100%] tracking-tight text-[#000000] font-medium">
+          Connect with Us
+        </h1>
+        <Link className="flex flex-row gap-2">
+          <img
+            src={Twitter}
+            alt="Twitter"
+            className="w-[24px] h-[22px] object-cover"
+          />
+          <span>X/Twitter</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
 export default Bottom;
--- src/Components/Landing/Rearny.jsx
+++ src/Components/Landing/Rearny.jsx
@@ -9,10 +9,9 @@ const Rearny = () => {
           <h1 className="w-[354px] h-[58px] text-[48px] leading-[100%] tracking-tight text-[#000000] font-medium">
             Ready To Earn?
           </h1>
-          <p className="w-[793px] h-[38px] text-[16px] leading-[100%] tracking-[0%] text-[#000000] font-normal text-center">
+          <p className="w-[783px] h-[38px] text-[16px] leading-[100%] tracking-[0%] text-[#000000] font-normal text-center">
             Whether you're looking to earn data rewards or scale your brand with
-            100% verified human engagement. <br />
-            TADAT is built for you.
+            100% verified human engagement. TADAT is built for you.
           </p>
           <button className="w-[261px] h-[53px] bg-[#FFFFFF] rounded-[15px] shadow-[0px_4px_4px_0px_#00000040] cursor-pointer hover:bg-[#FFFFFF]/80 flex justify-center items-center">
             <span className="bg-gradient-to-l from-[#30D5C7] to-[#0000FFE5] bg-clip-text text-transparent ">
