e0cdffdad68278570f35f5e82abd28b383046d8b
tenney_olarh <alameenadetona04@gmail.com>
2026-07-22 01:32:08 +0100

    imported and did routing for signup,login,dashboard.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import {Routes,Route} from 'react-router-dom';
 import Landing from './Pages/Landing';
-const App = () => {
+import Signup from './Pages/Signup';
+import Login from './Pages/Login';
+import Dashboard from './Pages/Dashboard';
+function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Landing />} />
-        </Routes>
-    )
-    }
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="login" element={<Login />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Routes>
+    );
+}
 export default App;
